9b6ea399aef997f7efdb4dedce6c903559f76a92
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-02-08 12:39:37 -0600

    Update make_submit_metplus_jobs.sh to be more general
    Updated script now works for both PointStat and GridStat rather than just GridStat.

--- make_submit_metplus_jobs.sh
+++ make_submit_metplus_jobs.sh
@@ -1,14 +1,23 @@
 
-INIT_BEG=2022042912
+# Looping uses INIT times rather than VALID times
+# To prevent weird things from happening, ensure that $VALID_BEG is within $step of $INIT_BEG
+INIT_BEG=2022042921
 INIT_END=2022050612
 
-fcst_name='spring_ctrl_grid'
-fcst_dir='/work2/noaa/wrfruc/murdzek/RRFS_OSSE/syn_data/spring'
-obs_dir='/work2/noaa/wrfruc/murdzek/nature_run_spring/UPP'
+VALID_BEG=2022043000
+VALID_END=2022050612
+
+step=160  # Recommended for PointStat
+#step=12    # Recommended for GridStat
+
+fcst_name='osse_spring_ctrl'
+fcst_dir='/work2/noaa/wrfruc/murdzek/RRFS_OSSE/syn_data_app_orion/spring'
+obs_dir='/work2/noaa/wrfruc/murdzek/RRFS_OSSE/metplus_verif_pt_obs/app_orion/obs_syn_with_errors/spring/output/pb2nc'
 
 metplusDIR='/work2/noaa/wrfruc/murdzek/src/metplus_OSSE_scripts'
-verif_templates=("${metplusDIR}/GridStat_sfc.conf" "${metplusDIR}/GridStat_ua.conf")
-verif_types=("sfc" "upper_air")
+verif_templates=("${metplusDIR}/PointStat_sfc.conf" "${metplusDIR}/PointStat_ua.conf")
+verif_type="PointStat"
+verif_subtypes=("sfc" "upper_air")
 user_template="${metplusDIR}/smurdzek_orion.conf"
 slurm_template="${metplusDIR}/run_metplus.sh"
 
@@ -16,20 +25,24 @@ slurm_template="${metplusDIR}/run_metplus.sh"
 
 parentDIR=`pwd`
 
-begin=${INIT_BEG}
-end=`date -d "${begin::8} ${begin:8:2} + 11 hours" +%Y%m%d%H`
+begin_init=${INIT_BEG}
+begin_valid=${VALID_BEG}
+hr_minus_one=$((step-1))
+end=`date -d "${begin_init::8} ${begin_init:8:2} + ${hr_minus_one} hours" +%Y%m%d%H`
 while [ ${end} -le ${INIT_END} ]; do
   for j in ${!verif_templates[@]}; do
-    echo "${begin}: ${verif_types[j]}"
-    dir=${parentDIR}/${verif_types[j]}/${begin}
+    echo "${begin_init}: ${verif_subtypes[j]}"
+    dir=${parentDIR}/${verif_subtypes[j]}/${begin_init}
     mkdir -p ${dir}/output
     cp ${metplusDIR}/metplus_orion.env ${dir}/
     
     # Create METplus configuration files. Use = as the sed delimiter b/c we are using paths that contain /
-    conf_fname=GridStat_${verif_types[j]}.conf
+    conf_fname=${verif_type}_${verif_subtypes[j]}.conf
     cp ${verif_templates[j]} ${dir}/${conf_fname}
-    sed -i "s={INIT_BEG}=${begin}=" ${dir}/${conf_fname}
+    sed -i "s={INIT_BEG}=${begin_init}=" ${dir}/${conf_fname}
     sed -i "s={INIT_END}=${end}=" ${dir}/${conf_fname}
+    sed -i "s={VALID_BEG}=${begin_valid}=" ${dir}/${conf_fname}
+    sed -i "s={VALID_END}=${end}=" ${dir}/${conf_fname}
     sed -i "s={FCST_DIR}=${fcst_dir}=" ${dir}/${conf_fname}
     sed -i "s={OBS_DIR}=${obs_dir}=" ${dir}/${conf_fname}
     sed -i "s={FCST_NAME}=${fcst_name}=" ${dir}/${conf_fname}
@@ -40,7 +53,7 @@ while [ ${end} -le ${INIT_END} ]; do
     sed -i "s={OUTPUT_BASE}=${dir}/output=" ${dir}/${user_fname}
   
     # Create and submit the slurm job submission file
-    slurm_fname=metplus_${verif_types[j]}_${begin}.sh 
+    slurm_fname=metplus_${verif_subtypes[j]}_${begin_init}.sh 
     cp ${slurm_template} ${dir}/${slurm_fname}
     sed -i "s={CONF_FILE}=./${conf_fname}=" ${dir}/${slurm_fname} 
     sed -i "s={USER_FILE}=./${user_fname}=" ${dir}/${slurm_fname} 
@@ -49,6 +62,7 @@ while [ ${end} -le ${INIT_END} ]; do
     cd ${parentDIR}
 
   done
-  begin=`date -d "${begin::8} ${begin:8:2} + 12 hours" +%Y%m%d%H`
-  end=`date -d "${end::8} ${end:8:2} + 12 hours" +%Y%m%d%H`
+  begin_init=`date -d "${begin_init::8} ${begin_init:8:2} + ${step} hours" +%Y%m%d%H`
+  begin_valid=`date -d "${begin_valid::8} ${begin_valid:8:2} + ${step} hours" +%Y%m%d%H`
+  end=`date -d "${end::8} ${end:8:2} + ${step} hours" +%Y%m%d%H`
 done
