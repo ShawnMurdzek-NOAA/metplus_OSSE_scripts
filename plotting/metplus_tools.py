e3b67d0f97dc91834200d33450f61efc7fb87f3d
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2024-06-28 14:28:09 -0500

    remove delim_whitespace when reading CSVs with pandas
    delim_whitespace keyword is deprecated. Now use a different approach suggested by pandas

--- plotting/metplus_tools.py
+++ plotting/metplus_tools.py
@@ -161,7 +161,7 @@ def read_ascii(fnames, verbose=True):
     raw_dfs = []
     for f in fnames:
         try:
-            df = pd.read_csv(f, delim_whitespace=True)
+            df = pd.read_csv(f, sep='\s+')
             if len(df) > 0:
                 raw_dfs.append(df)
         except FileNotFoundError:
